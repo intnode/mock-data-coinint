fa649825608abffb0f881ee424214b6072ae2548
PeemapatW <peemapat.w@gmail.com>
2023-05-16 15:02:38 +0700

    add root-path arg

--- main.py
+++ main.py
@@ -44,15 +44,16 @@ if __name__ == "__main__":
     """
     parser = argparse.ArgumentParser(description='Run API for backtesting')
     parser.add_argument('--host', type=str, help='host, [default=127.0.0.1]', default="127.0.0.1")
+    parser.add_argument('--root-path' ,type=str, help='Set the ASGI root_path for applications submounted below a given URL path', default="")
     parser.add_argument('--port', type=int, help='port, [default=6600]', default=6600)
     parser.add_argument('--workers', type=int, help='number of workers, [default=1]', default=1)
     parser.add_argument('--debug', type=str2bool, nargs='?', const=True, help='debug mode, [default=False]', default=False)
     parser.add_argument('--updater' ,type=str2bool, nargs='?', const=True, help='run db updating service in background, [default=True]', default=True)
-    args = parser.parse_args() 
+    args = parser.parse_args()
 
     if args.debug:
         print(f"Running API in debug mode")
-        uvicorn.run("prototype_api:app", host="127.0.0.1", port=6600, reload=args.debug)
+        uvicorn.run("prototype_api:app", host="127.0.0.1", port=6600, reload=args.debug, root_path=args.root_path)
     else:
         if args.updater:
             print(f"Updating database before starting API")
@@ -64,7 +65,7 @@ if __name__ == "__main__":
             # schedule.run_all()   
             print(f"Updating is completed, start to running the API")
             run_background_schedule = run_continuously() # Make all schedules updating run in a background
-            uvicorn.run("prototype_api:app", host=args.host, port=args.port, workers=args.workers)
+            uvicorn.run("prototype_api:app", host=args.host, port=args.port, workers=args.workers, root_path=args.root_path)
             run_background_schedule.set()
         else:
-            uvicorn.run("prototype_api:app", host=args.host, port=args.port, workers=args.workers)
+            uvicorn.run("prototype_api:app", host=args.host, port=args.port, workers=args.workers, root_path=args.root_path)
